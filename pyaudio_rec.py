7e7dd185fffce5a6dd8984430741eda935c95bec
Uncle Engineer <eng.imron@gmail.com>
2019-05-01 21:37:17 +0700

    Add files via upload

---
+++ pyaudio_rec.py
@@ -0,0 +1,48 @@
+#pip install pyaudio for 3.6
+
+import pyaudio
+import wave
+import subprocess
+
+
+def Record(filename='test',second=5):
+	CHUNK = 1024
+	FORMAT = pyaudio.paInt16
+	CHANNELS = 2
+	RATE = 44100
+	RECORD_SECONDS = second
+	WAVE_OUTPUT_FILENAME = filename + ".wav"
+
+	p = pyaudio.PyAudio()
+
+	stream = p.open(format=FORMAT,
+	                channels=CHANNELS,
+	                rate=RATE,
+	                input=True,
+	                frames_per_buffer=CHUNK)
+
+	print("* recording")
+
+	frames = []
+
+	for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+	    data = stream.read(CHUNK)
+	    frames.append(data)
+
+	print("* done recording")
+
+	stream.stop_stream()
+	stream.close()
+	p.terminate()
+
+	wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+	wf.setnchannels(CHANNELS)
+	wf.setsampwidth(p.get_sample_size(FORMAT))
+	wf.setframerate(RATE)
+	wf.writeframes(b''.join(frames))
+	wf.close()
+
+
+Record('test',5)
+
+subprocess.Popen('test.wav',shell=True)
